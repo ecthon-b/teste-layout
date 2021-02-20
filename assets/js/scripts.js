5f93c280098bb6120d072e5b6e510f7d71011dbe
Ecthon Borhis <ecthon@gmail.com>
2021-02-20 09:25:19 -0400

    Start Escalator project

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.body',{}); 
+sr.reveal('.home',{delay: 100}); 
+sr.reveal('.categories',{delay: 100}); 
+sr.reveal('.plans',{ delay: 100}); 
